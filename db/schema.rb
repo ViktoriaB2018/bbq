efb204348032d4e0529c08e7b6c644ed13f2e5c0
ViktoriaB2018 <bvo2008@mail.ru>
2019-02-25 19:28:55 +0200

    Users initial

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_162523) do
+ActiveRecord::Schema.define(version: 2019_02_25_171442) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "address"
     t.datetime "datetime"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
