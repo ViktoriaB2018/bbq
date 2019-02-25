78d9c0cc3027d7480eff3f39aeece0804ba2a984
ViktoriaB2018 <bvo2008@mail.ru>
2019-02-25 20:05:08 +0200

    Add relation user-events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_171442) do
+ActiveRecord::Schema.define(version: 2019_02_25_173225) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_02_25_171442) do
     t.datetime "datetime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
