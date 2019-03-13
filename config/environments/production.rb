ef267a7d73f62acd717ad8a1c877e1ca4ae4cf64
ViktoriaB2018 <bvo2008@mail.ru>
2019-03-13 11:45:43 +0200

    Fix bag show e-mail address in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,8 +94,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'let-bbq.herokuapp.com' }
 
-  config.action_mailer.default_options = { from: 'let-bbq.herokuapp.com' }
-
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_deliveries = true
